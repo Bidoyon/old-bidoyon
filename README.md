5e33fc20cab0bdd6552f9fa3d9fb732a23a1a4da
Bidulman <63709148+Bidulman@users.noreply.github.com>
2021-10-26 17:55:33 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# jus-de-pomme-web
-  Une application Web pour gérer l'événement "Jus de Pomme" sans trop se casser la tête ! (Application Web)
+# Jus de Pomme
+Une application Web pour gérer l'événement "Jus de Pomme" sans trop se casser la tête !
+
+## Présentation
+
+### Organisation
+Le projet est divisé en deux parties :
+- L'API, qui contient toutes les données et les distribue
+- L'application Web, qui est l'interface à laquelle les utilisateurs peuvent accéder
+
+Les principaux outils utilisés sont :
+- Python, nécessaire au fonctionnement des deux parties
+- FastAPI, utilisé pour la création de l'API
+- Flask, utilisé pour la création de l'application Web
+
+Le projet a été créé, imaginé et développé par Bidulman dans le cadre de l'événement "Jus de Pomme", où des personnes amènent des pommes (comptées en Kg), les pressent et doivent repartir avec une dose de jus de pomme (comptée en L) proportionnelle à la quantité de pommes amenée, ainsi que la rentabilité des pommes (comptée en L/Kg).
+
+
+### But du projet
+- "Page d'Accueil" qui permet à tout le monde de visualiser les pressées
+- "Connexion" qui donne accès à un système d'authentification pour se connecter en tant qu'admin, manager ou investor
+- "Panel Admin" pour les admin, où nous pouvons créer et supprimer des utilisateurs
+- "Panel Manager" pour les managers, où nous pouvons gérer les pressées (pommes utilisées, jus produit)
+- "Panel Investor"
+  - pour les investors, qui peuvent depuis ici visualiser combien ils peuvent emporter de jus à la fin des pressages
+  - pour les managers, qui peuvent voir combien de jus de pomme doit être redistribué
+
+### Utilisation
+Bien que le projet soit ouvert à tous, il n'a été développé que pour son propriétaire. Le projet ne sera donc pas documenté sur la partie "déploiement".
+
+
+## Conclusion
+
+Le projet a donc été développé dans le but de décharger les reponsabilités de "l'agent comptable" de l'événement "Jus de Pomme", qui n'a maintenant plus qu'à entrer des nombres sur son téléphone portable, sans besoin de calculer. On appelle aussi ça "la flemme", mais ça ne faisait pas très beau dans une présentation de projet sérieux.
---
+++ web/app.py
@@ -0,0 +1,172 @@
+from flask import Flask, render_template, request, redirect
+
+import requests
+import json
+
+app = Flask(__name__)
+address = "http://localhost:5000"
+api_address = "http://localhost:8000"
+
+
+def message(text, panel, token):
+    return render_template('message.html', text=text, address=address, panel=panel, token=token)
+
+
+@app.route('/')
+def index():
+
+    response = requests.get(f"{api_address}/pressings")
+
+    pressings = response.json()
+
+    current = pressings[len(pressings)-1]
+
+    total_juice = 0
+    total_apples = 0
+
+    for pressing in pressings:
+        total_juice += pressing[1]
+        total_apples += pressing[2]
+
+    total_juice_per_apples = total_juice/total_apples if total_juice and total_apples else "Impossible de calculer "
+
+    pressings.pop()
+
+    pressings.reverse()
+
+    return render_template('index.html',
+                           current_number=current[0],
+                           current_juice=current[1],
+                           current_apples=current[2],
+                           current_juice_per_apples=current[3],
+                           total_juice=total_juice,
+                           total_apples=total_apples,
+                           total_juice_per_apples=total_juice_per_apples,
+                           last_pressings=pressings)
+
+
+@app.route('/login', methods=["GET"])
+def get_login():
+    return render_template('login.html', address=address)
+
+
+@app.route('/login', methods=["POST"])
+def post_login():
+
+    username = request.form['username']
+    password = request.form['password']
+
+    response = requests.get(f"{api_address}/users", data=json.dumps({"username": username, "password": password}))
+
+    if response.status_code == 200:
+
+        permission = response.json()['permission']
+
+        if permission == 'admin':
+
+            return redirect(f"{address}/admin?token={response.json()['token']}")
+
+        elif permission == 'manager':
+
+            return redirect(f"{address}/manager?token={response.json()['token']}")
+
+    return "No you don't"
+
+
+@app.route('/manager')
+def manager():
+    token = request.args.get('token')
+
+    response = requests.get(f"{api_address}/tokens", data=json.dumps({"token": token}))
+
+    if response.status_code == 200 and (response.json()['permission'] == 'manager' or response.json()['permission'] == 'admin'):
+
+        nb_pressings = len(requests.get(f"{api_address}/pressings").json())
+
+        return render_template('manager.html', nb_pressings=nb_pressings, token=token)
+
+    return "NO YOU DON'T"
+
+
+@app.route('/admin')
+def admin():
+
+    token = request.args.get('token')
+
+    response = requests.get(f"{api_address}/tokens", data=json.dumps({"token": token}))
+
+    if response.status_code == 200 and response.json()['permission'] == 'admin':
+
+        return render_template('admin.html', token=token)
+
+    return "NO YOU DON'T"
+
+
+@app.route('/users', methods=["POST"])
+def add_remove_user():
+
+    token = request.args.get('token')
+
+    response = requests.get(f"{api_address}/tokens", data=json.dumps({"token": token}))
+
+    if response.status_code == 200 and response.json()['permission'] == 'admin':
+
+        if request.args.get('action') == "add":
+
+            print(request.form['permission'])
+
+            response = requests.post(f"{api_address}/users", data=json.dumps({"username": request.form['username'], "password": request.form['password'], "permission": request.form['permission']}))
+
+            if response.status_code == 200:
+
+                return message("Vous avez bien créé l'utilisateur.", 'admin', token)
+
+            else:
+
+                return message(f"L'utilisateur n'a pas pu être créé ({response.status_code} {response.reason}).", 'admin', token)
+
+        elif request.args.get('action') == "delete":
+
+            response = requests.delete(f"{api_address}/users", data=json.dumps({"username": request.form['username']}))
+
+            if response.status_code == 200:
+
+                return message("Vous avez bien supprimé l'utilisateur.", 'admin', token)
+
+            else:
+
+                return message(f"L'utilisateur n'a pas pu être supprimé ({response.status_code} {response.reason}).", 'admin', token)
+
+
+@app.route('/pressings', methods=["POST"])
+def add_edit_pressing():
+
+    token = request.args.get('token')
+
+    response = requests.get(f"{api_address}/tokens", data=json.dumps({"token": token}))
+
+    if response.status_code == 200 and (response.json()['permission'] == 'manager' or response.json()['permission'] == 'admin'):
+
+        if request.args.get('action') == 'add':
+
+            response = requests.post(f"{api_address}/pressings", data=json.dumps({"added_juice": request.form['juice'], "added_apples": request.form['apples']}))
+
+            if response.status_code == 200:
+
+                return message("Vous avez bien créé une nouvelle pressée !", 'manager', token)
+
+        if request.args.get('action') == 'edit':
+
+            number = int(request.form['number'])
+            added_juice = int(request.form['added_juice'])
+            added_apples = int(request.form['added_apples'])
+
+            response = requests.post(f"{api_address}/pressings", data=json.dumps({"number": number, "added_juice": added_juice, "added_apples": added_apples}))
+
+            if response.status_code == 200:
+
+                return message("Vous avez bien modifié la pressée !", 'manager', token)
+
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", 5000, debug=True)
